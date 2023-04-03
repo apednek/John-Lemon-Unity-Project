382b24d6eb7e14bb04088794fd7a2f0272717478
Pednekar, Aditi Shivaji <adi410pednekar@gmail.com>
2023-04-02 20:28:55 -0400

    Final

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public GameObject player;
+    public float fadeDuration = 1f;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public float displayImageDuration = 1f;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+
+
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+    bool m_IsPlayerCaught;
+    bool m_HasAudioPlayed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if(!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {  
+            if(doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
